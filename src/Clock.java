f453301adbf93ac6f1ee775f28f5a9ad51487222
Eleanor BW <ebarthwu@gmail.com>
2019-09-13 10:35:52 -0400

    Finished clock code with modulus!

--- src/Clock.java
+++ src/Clock.java
@@ -6,7 +6,7 @@ public class Clock {
 		//give an ending time
 		
 		//get start time as a string
-		String answer = JOptionPane.showInputDialog(null, "Give me a start time (ex: 3:10)");
+		String answer = JOptionPane.showInputDialog(null, "When does this even start? (ex: 3:10): ");
 		//figure out where the colon in
 		int colonInd = answer.indexOf(':');
 		//take start of string to where the colon is and make it an integer
@@ -16,6 +16,61 @@ public class Clock {
 		//check that it came in correctly
 		//System.out.println("Hours: " + hours);
 		//System.out.println("Minutes: " + minutes);
+		
+		String input = JOptionPane.showInputDialog(null, "How long is this event in minutes: ");
+		int length = Integer.parseInt(input);
+		//System.out.println(length);
+
+		int hourRemains = length / 60;
+		//System.out.println(hourRemains);
+		
+		int minRemains = length % 60;
+		//System.out.println(minRemains);
+		
+		int newHour = hours + hourRemains;
+		//System.out.println(newHour);
+		int newMin = minutes + minRemains;
+		
+		if (newMin > 60) 
+		{
+			int brandNewHour = newMin / 60;
+			int finalHours = newHour + brandNewHour;
+			//System.out.println(finalHours);
+			int brandNewMin = newMin % 60;
+			if (brandNewMin < 10)
+				System.out.println("The event ends at: " + finalHours + ":0" + brandNewMin);
+			else {
+				System.out.println("The event ends at: " + finalHours + ":" + brandNewMin);
+			}
+		}	
+		else if (newMin < 10)
+		{
+			System.out.println("The event ends at: " + newHour + ":0" + newMin);
+		}
+		
+		else 
+		{
+				System.out.println("The event ends at: " + newHour + ":" + newMin);
+		}
+		
+		
+		
+			
+		
+		
+		//
+		
+		//find the total minutes
+		//find modulus of the total minutes
+		
+		
+		/*if (newMin > 60)
+		{
+			int newMinRemains = newMin / 60;
+			newHour = hourRemains + newMinRemains;
+			int mins = newMin % 60;
+			int leftOverMin = mins + newMin;
+		}*/
 		
 		
 		
